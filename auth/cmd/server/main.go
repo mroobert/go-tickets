c8d38d0409ba64df66f3dfbcfb43794fff12a2dc
mroobert <robertmihairo94@gmail.com>
2022-03-02 21:24:13 +0200

    AUTH - layout: initial commit for vertical arch

--- auth/cmd/server/main.go
+++ auth/cmd/server/main.go
@@ -1,46 +1,100 @@
 package main
 
 import (
+	"context"
+	"expvar"
 	"fmt"
+	"os"
+	"runtime"
 	"time"
 
+	firebase "firebase.google.com/go/v4"
+	"github.com/mroobert/go-tickets/auth/internal/foundation/logger"
 	"github.com/spf13/viper"
+	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
 )
-
-type config struct {
-	Version         string        `mapstructure:"VERSION"`
-	APIHost         string        `mapstructure:"API_HOST"`
-	DebugHost       string        `mapstructure:"DEBUG_HOST"`
-	ReadTimeout     time.Duration `mapstructure:"READ_TIMEOUT"`
-	WriteTimeout    time.Duration `mapstructure:"WRITE_TIMEOUT"`
-	IdleTimeout     time.Duration `mapstructure:"IDLE_TIMEOUT"`
-	ShutdownTimeout time.Duration `mapstructure:"SHUTDOWN_TIMEOUT"`
-}
-
-var cfg *config
 
 var build = "develop"
 
-func init() {
-	viper.AddConfigPath(".")
+func main() {
+	// Construct the application logger.
+	log, err := logger.New("AUTH-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	// =========================================================================
+	// GOMAXPROCS
+
+	// Set the correct number of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+
+	// =========================================================================
+	// Configuration
+
+	cfg := struct {
+		Version string `mapstructure:"VERSION"`
+		Web     struct {
+			APIHost         string        `mapstructure:"API_HOST"`
+			DebugHost       string        `mapstructure:"DEBUG_HOST"`
+			ReadTimeout     time.Duration `mapstructure:"READ_TIMEOUT"`
+			WriteTimeout    time.Duration `mapstructure:"WRITE_TIMEOUT"`
+			IdleTimeout     time.Duration `mapstructure:"IDLE_TIMEOUT"`
+			ShutdownTimeout time.Duration `mapstructure:"SHUTDOWN_TIMEOUT"`
+		}
+	}{
+		Version: build,
+	}
+
+	viper.AddConfigPath("./cmd/server/")
 	viper.SetConfigName("server")
-	viper.SetConfigType("env")
-	viper.AutomaticEnv()
+	viper.SetConfigType("json")
 
 	err := viper.ReadInConfig()
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
 
-	cfg = &config{}
-	err = viper.Unmarshal(cfg)
+	err = viper.Unmarshal(&cfg)
 	if err != nil {
 		fmt.Printf("unable to decode into config struct, %v", err)
 	}
-}
 
-func main() {
-	fmt.Println("")
-	fmt.Println(build)
-	fmt.Println(cfg)
+	// =========================================================================
+	// App Starting
+
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "config", cfg)
+	defer log.Infow("shutdown complete")
+
+	expvar.NewString("build").Set(cfg.Version)
+
+	// =========================================================================
+	// Initialize Firebase Support
+
+	fbClient, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		return fmt.Errorf("error initializing firebase client: %w", err)
+	}
+
+	fbAuthClient, err := fbClient.Auth(context.Background())
+	if err != nil {
+		return fmt.Errorf("error initializing firebase auth client: %w", err)
+	}
+
+	fmt.Println(fbAuthClient)
+	return nil
 }
---
+++ auth/user.go
@@ -0,0 +1,7 @@
+package auth
+
+type User struct {
+	DisplayName string
+	Email       string
+	Password    string
+}
